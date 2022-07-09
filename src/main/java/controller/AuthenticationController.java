d9b51a6bb76127ac20301b806baaea346752e97c
dinhhh <dinhhung.0115@gmail.com>
2022-07-09 08:11:01 +0700

    refactor class AuthenticationController by extract method md5()

--- src/main/java/controller/AuthenticationController.java
+++ src/main/java/controller/AuthenticationController.java
@@ -4,16 +4,11 @@ import common.exception.ExpiredSessionException;
 import common.exception.FailLoginException;
 import dao.user.UserDAO;
 import entity.user.User;
-import utils.Utils;
+import utils.MessageDigestUtils;
 
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.Objects;
-
-
 
 /**
  * @author
@@ -30,16 +25,22 @@ public class AuthenticationController extends BaseController {
     }
 
     public User getMainUser() throws ExpiredSessionException {
-        if (SessionInformation.mainUser == null || SessionInformation.expiredTime == null || SessionInformation.expiredTime.isBefore(LocalDateTime.now())) {
+        if (SessionInformation.mainUser == null
+                || SessionInformation.expiredTime == null
+                || SessionInformation.expiredTime.isBefore(LocalDateTime.now())) {
             logout();
             throw new ExpiredSessionException();
-        } else return SessionInformation.mainUser.cloneInformation();
+        } else {
+            return SessionInformation.mainUser.cloneInformation();
+        }
     }
 
     public void login(String email, String password) {
         try {
-            User user = new UserDAO().authenticate(email, genDigestByMd5(password));
-            if (Objects.isNull(user)) throw new FailLoginException();
+            User user = new UserDAO().authenticate(email, MessageDigestUtils.genDigestByMd5(password));
+            if (Objects.isNull(user)) {
+                throw new FailLoginException();
+            }
             SessionInformation.mainUser = user;
             SessionInformation.expiredTime = LocalDateTime.now().plusHours(24);
         } catch (SQLException ex) {
@@ -50,35 +51,6 @@ public class AuthenticationController extends BaseController {
     public void logout() {
         SessionInformation.mainUser = null;
         SessionInformation.expiredTime = null;
-    }
-
-    
-    // Co inacidental
-    /**
-     * Return a {@link String String} that represents the cipher text
-     * encrypted by md5 algorithm.
-     *
-     * @param message - plain text as {@link String String}.
-     * @return cipher text as {@link String String}.
-     */
-     // Vi phạm SRP: Không phải nhiệm vụ của lớp này
-     // Sau này có thể sử dụng hàm băm khác...
-    private String genDigestByMd5(String message) {
-        String digest = null;
-        try {
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            byte[] hash = md.digest(message.getBytes(StandardCharsets.UTF_8));
-            // converting byte array to Hexadecimal String
-            StringBuilder sb = new StringBuilder(2 * hash.length);
-            for (byte b : hash) {
-                sb.append(String.format("%02x", b & 0xff));
-            }
-            digest = sb.toString();
-        } catch (NoSuchAlgorithmException ex) {
-            Utils.getLogger(Utils.class.getName());
-            digest = "";
-        }
-        return digest;
     }
 
 }
---
+++ src/main/java/utils/MessageDigestUtils.java
@@ -0,0 +1,27 @@
+package utils;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MessageDigestUtils {
+
+    public static String genDigestByMd5(String message) {
+        String digest;
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] hash = md.digest(message.getBytes(StandardCharsets.UTF_8));
+            // converting byte array to Hexadecimal String
+            StringBuilder sb = new StringBuilder(2 * hash.length);
+            for (byte b : hash) {
+                sb.append(String.format("%02x", b & 0xff));
+            }
+            digest = sb.toString();
+        } catch (NoSuchAlgorithmException ex) {
+            Utils.getLogger(Utils.class.getName());
+            digest = "";
+        }
+        return digest;
+    }
+
+}
